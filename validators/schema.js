4543f7e67c227e790c245d552851f3eb6e093fa9
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-10-14 01:21:16 +0300

    - remove redundant SchemaValidator constructor

--- validators/schema.js
+++ validators/schema.js
@@ -3,10 +3,6 @@
 const GenericValidator = require('./generic')
 
 module.exports = class SchemaValidator extends GenericValidator {
-  constructor(schemaCheck) {
-    super(schemaCheck)
-  }
-
   getShapeValidator() {
     throw new Error('abstract method not call')
   }
