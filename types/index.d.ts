37fdb5adecd3de5201c0cda63215002d71b9060c
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-10-30 22:40:27 +0300

    add missed type definition of `object` and `array`

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,8 +2,9 @@ import {CheckDto} from "./interfaces";
 import GenericValidator from "./validators/generic";
 import StringValidator from "./validators/string";
 import NumberValidator from "./validators/number";
-import ObjectValidator from "./validators/object";
-import ArrayValidator from "./validators/array";
+import ObjectValidator, {ObjectShape} from "./validators/object";
+import ArrayValidator, { ArrayShape } from "./validators/array";
+import {ValidatorCreator} from "./validators/schema";
 
 export { GenericValidationError, ValidationError, BulkValidationError } from './errors'
 
@@ -28,6 +29,6 @@ export function string(): StringValidator
 
 export function number(): NumberValidator
 
-export function object(): ObjectValidator
+export function object(value: ObjectShape | ValidatorCreator | GenericValidator): ObjectValidator
 
-export function array(): ArrayValidator
+export function array(value: ArrayShape | ValidatorCreator | GenericValidator): ArrayValidator
