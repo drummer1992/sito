8cdfaad8766b623d2c69c0e34a0ea1b0ca897a52
Andrii Varlamov <andrii.varlamov@backendless.consulting>
2021-10-31 12:49:48 +0200

    - fix API types

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,44 +3,42 @@ import GenericValidator from "./validators/generic";
 import StringValidator from "./validators/string";
 import NumberValidator from "./validators/number";
 import ObjectValidator, {ObjectShape} from "./validators/object";
-import ArrayValidator, { ArrayShape } from "./validators/array";
+import ArrayValidator, {ArrayShape} from "./validators/array";
 import {ValidatorCreator} from "./validators/schema";
 
-export { GenericValidationError, ValidationError, BulkValidationError } from './errors'
+export {GenericValidationError, ValidationError, BulkValidationError} from './errors'
 
-export { default as GenericValidator } from './validators/generic';
-export { default as SchemaValidator } from './validators/schema';
-export { default as StringValidator } from './validators/string';
-export { default as NumberValidator } from './validators/number';
-export { default as ObjectValidator } from './validators/object';
-export { default as ArrayValidator } from './validators/array';
+export {default as GenericValidator} from './validators/generic';
+export {default as SchemaValidator} from './validators/schema';
+export {default as StringValidator} from './validators/string';
+export {default as NumberValidator} from './validators/number';
+export {default as ObjectValidator} from './validators/object';
+export {default as ArrayValidator} from './validators/array';
 
-declare namespace Sito {
-    function check(check: CheckDto): GenericValidator
+export function check(check: CheckDto): GenericValidator
 
-    function required(enabled?: boolean): GenericValidator
+export function required(enabled?: boolean): GenericValidator
 
-    function forbidden(enabled?: boolean): GenericValidator
+export function forbidden(enabled?: boolean): GenericValidator
 
-    function exists(enabled?: boolean): GenericValidator
+export function exists(enabled?: boolean): GenericValidator
 
-    function oneOf(values: any[]): GenericValidator
+export function oneOf(values: any[]): GenericValidator
 
-    function string(): StringValidator
+export function string(): StringValidator
 
-    function number(): NumberValidator
+export function number(): NumberValidator
 
-    function object(shape: ObjectShape): ObjectValidator
+declare function object(shape: ObjectShape): ObjectValidator
 
-    function object(validatorCreator: ValidatorCreator): ObjectValidator
+declare function object(validatorCreator: ValidatorCreator): ObjectValidator
 
-    function object(validator: GenericValidator): ObjectValidator
+declare function object(validator: GenericValidator): ObjectValidator
 
-    function array(shape: ArrayShape): ArrayValidator
+declare function array(shape: ArrayShape): ArrayValidator
 
-    function array(validatorCreator: ValidatorCreator): ArrayValidator
+declare function array(validatorCreator: ValidatorCreator): ArrayValidator
 
-    function array(validator: GenericValidator): ArrayValidator
-}
+declare function array(validator: GenericValidator): ArrayValidator
 
-export default Sito
+export {object, array}
