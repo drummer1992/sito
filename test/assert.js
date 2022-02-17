5bc35807f2a9148ef868efd091ff39505ff614e1
Andrey Varlamov <andrii.varlamov@backendless.consulting>
2022-02-17 20:43:59 +0200

    [assert] do not return anything in case validation passed

--- test/assert.js
+++ test/assert.js
@@ -8,4 +8,10 @@ describe('assert', () => {
 
     return assert.rejects(schema.assert({}), /foo is required/)
   })
+
+  it('should return undefined in case validation passed', async () => {
+    const schema = object({ foo: string().required() })
+
+    assert.strictEqual(await schema.assert({ foo: '' }), undefined)
+  })
 })
