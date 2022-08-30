937a81738c739740e2d9139072ae3e352f162f6b
sstratonov <ostratonov@shipadelivery.com>
2022-08-30 19:22:31 +0300

    uuid4 support

--- test/string.js
+++ test/string.js
@@ -14,14 +14,32 @@ describe('string', () => {
 
     const validationSchema = object({
       email: string().required().pattern(EMAIL_REGEX)
-          .combine(),
+        .combine(),
       displayName: string(),
       password: string(),
     })
 
     await assert.rejects(
-        validationSchema.assert({ email: 'mail' }),
-        /email does not match the pattern/,
+      validationSchema.assert({ email: 'mail' }),
+      /email does not match the pattern/,
+    )
+  })
+
+  it('uuid4', async () => {
+    const uuid4 = 'edf1f073-0615-4555-aab3-85238d249d2b'
+    const uuid4Uppercase = 'faf4e3e9-3bbf-4d57-b7cb-40a2deda674c'.toUpperCase()
+    const randomString = '532234-432424-2343-4234-5324-2353453'
+
+    assert.strictEqual(await string().uuid4().isValid(uuid4), true)
+    assert.strictEqual(await string().uuid4().isValid(uuid4Uppercase), true)
+
+    const validationSchema = object({
+      objectId: string().required().uuid4(),
+      name: string(),
+    })
+
+    return assert.rejects(
+      validationSchema.assert({ objectId: randomString }),
     )
   })
 })
